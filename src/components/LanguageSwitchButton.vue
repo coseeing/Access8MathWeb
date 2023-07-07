419918e8a75e01d679ae5e2163a0ba641703bbde
YC Chen <yo7chen@gmail.com>
2023-07-07 19:51:17 +0800

    fix(i18n):correct locale setting after refresh page

--- src/components/LanguageSwitchButton.vue
+++ src/components/LanguageSwitchButton.vue
@@ -29,34 +29,22 @@
 </template>
 <script setup>
 import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
-import router from "../router/index.js";
-import createI18n, { loadLocaleMessages } from "../i18n.js";
+import { watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
 import { useI18n } from "vue-i18n";
 const SUPPORTED_LOCALE_OPTIONS = Object.freeze([
-	{ label: "TC 中文", value: "zh" },
+	{ label: "TC 中文", value: "" },
 	{ label: "EN English", value: "en" },
 ]);
-
-const { locales, messages } = loadLocaleMessages();
 let { locale } = useI18n();
+const route = useRoute();
+const router = useRouter();
 
 const changeLanguage = (lang) => {
 	// change root locale
 	locale.value = lang;
 	// change router param
 	router.push({ params: { locale: lang } });
-	// Hot updates
-	if (module.hot) {
-		module.hot.accept(locales.id, () => {
-			const { messages: newMessages } = loadLocaleMessages();
-			console.log(newMessages);
-			Object.keys(newMessages)
-				.filter((locale) => messages[locale] !== newMessages[locale])
-				.forEach((locale) => {
-					messages[locale] = newMessages[locale];
-					createI18n.setLocaleMessage(locale, messages[locale]);
-				});
-		});
-	}
 };
+watch(() => route.params, changeLanguage(route.params.locale));
 </script>
