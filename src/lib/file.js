26a6917d57024f24865dd3dc5740b273ab12590d
Wendell Liu <wendell.liu@houzz.com>
2023-11-28 19:27:25 +0800

    Import action (#12)
    * feat: input action
    
    * feat: input action
    
    * feat: error handling

---
+++ src/lib/file.js
@@ -0,0 +1,24 @@
+export function getFileDataAsText(file) {
+  return new Promise(function (resolve, reject) {
+    const reader = new FileReader();
+
+    if (!(file instanceof Blob)) {
+      reject(new Error('The input is not a Blob.'));
+    }
+
+    reader.readAsText(file);
+
+    reader.onload = (e) => {
+      resolve(e.target.result);
+    };
+
+    reader.onerror = (e) => {
+      console.error(e);
+      reject(e);
+    };
+
+    reader.onabort = (e) => {
+      console.log('onabort', e);
+    };
+  });
+}
