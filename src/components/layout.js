c59f50434f51807eca323c18a17cd31a19ee79d0
jamliaoo <tzuhualiao@gmail.com>
2025-06-24 15:58:30 +0800

    Integrate Toaster into main layout
    Add the <Toaster /> component to the root Layout, making the notification system globally available throughout the application.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { LocaleContextProvider } from '@/lib/locale-switch';
 
 import Header from './header';
+import Toaster from './core/toast';
 
 const Layout = () => {
   return (
     <LocaleContextProvider>
-      <div>
-        <Header />
+      <Fragment>
+        <div>
+          <Header />
 
-        <main className="pt-20 md:h-screen w-screen ">
-          <Outlet />
-        </main>
-      </div>
+          <main className="pt-20 md:h-screen w-screen ">
+            <Outlet />
+          </main>
+        </div>
+        <Toaster />
+      </Fragment>
     </LocaleContextProvider>
   );
 };
