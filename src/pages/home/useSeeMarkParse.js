7d129f3fdb0e8f5960a1a573af4f45508b676450
Wendell Liu <cuk.bas@gmail.com>
2025-07-29 12:25:49 +0800

    feat: integrate with internal link

--- src/pages/home/useSeeMarkParse.js
+++ src/pages/home/useSeeMarkParse.js
@@ -3,6 +3,7 @@ import { useCallback } from 'react';
 import { createMarkdownToReactParser } from '@coseeing/see-mark';
 
 import Alert from '@/components/parser-components/alert/alert';
+import InternalLink from '@/components/parser-components/internal-link/inetrnal-link';
 
 const useSeeMarkParse = ({ latexDelimiter, htmlMathDisplay, imageFiles }) => {
   const seeMarkReactParse = useCallback(
@@ -13,7 +14,7 @@ const useSeeMarkParse = ({ latexDelimiter, htmlMathDisplay, imageFiles }) => {
           htmlMathDisplay,
           imageFiles,
         },
-        components: { alert: Alert },
+        components: { alert: Alert, internalLink: InternalLink },
       })(markdown);
     },
     [imageFiles, latexDelimiter, htmlMathDisplay]
