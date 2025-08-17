b0d703d30f2ef46303622b7b3f8c6e757a9d0d0b
jamliaoo <tzuhualiao@gmail.com>
2025-08-17 17:35:34 +0800

    Init storybook

---
+++ .storybook/main.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-docs',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-themes',
+  ],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  staticDirs: ['../public'],
+  typescript: {
+    reactDocgen: 'react-docgen',
+  },
+  webpackFinal: async (config) => {
+    // Add support for @ alias
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src'),
+    };
+    return config;
+  },
+};
+export default config;
---
+++ .storybook/preview.js
@@ -0,0 +1,27 @@
+import { withThemeByClassName } from '@storybook/addon-themes';
+
+// Import Tailwind CSS
+import '../src/index.css';
+
+/** @type { import('@storybook/react-webpack5').Preview } */
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: '',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+    }),
+  ],
+};
+
+export default preview;
